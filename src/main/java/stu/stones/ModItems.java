9a2423c5ea7437fd4ac2d378382b8ee399cee217
Stu <84202775+Sturext@users.noreply.github.com>
2025-03-05 23:14:06 -0600

    Add files via upload

---
+++ src/main/java/stu/stones/ModItems.java
@@ -0,0 +1,57 @@
+package stu.stones;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item THUNDERSTONE = registerItem("thunderstone", Item::new,  new Item.Settings().maxCount(64));
+    public static final Item FIRESTONE = registerItem("firestone", Item::new,  new Item.Settings().maxCount(64));
+    public static final Item WATERSTONE = registerItem("waterstone", Item::new,  new Item.Settings().maxCount(64));
+    public static final Item WINDSTONE = registerItem("windstone", Item::new,  new Item.Settings().maxCount(64));
+    public static final Item THUNDERSTAFF =  registerItem("thunderstaff",ThunderStaffItem::new, new Item.Settings().maxDamage(32));
+    public static final Item FIRESTAFF =  registerItem("firestaff",FireStaffItem::new, new Item.Settings().maxDamage(64));
+    public static final Item WATERSTAFF =  registerItem("waterstaff",WaterStaffItem::new, new Item.Settings().maxDamage(64));
+    public static final Item WINDSTAFF =  registerItem("windstaff",WindStaffItem::new, new Item.Settings().maxDamage(64));
+
+
+    public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings){
+        final RegistryKey<Item> registerKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("stones",name));
+        return Items.register(registerKey,factory,settings);
+    }
+
+    public static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings){
+        final RegistryKey<Block> registerKey = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of("stones",name));
+        Block freshBlock = Blocks.register(registerKey,factory,settings);
+        Items.register(freshBlock);
+        return freshBlock;
+    }
+
+    private static void customIngredients(FabricItemGroupEntries entries){
+        entries.add(THUNDERSTONE);
+        entries.add(FIRESTONE);
+        entries.add(WATERSTONE);
+        entries.add(WINDSTONE);
+        entries.add(THUNDERSTAFF);
+        entries.add(FIRESTAFF);
+        entries.add(WATERSTAFF);
+        entries.add(WINDSTAFF);
+    }
+
+    public static void registerModItems(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModItems::customIngredients);
+    }
+
+
+
+}
---
+++ src/main/java/stu/stones/WindStaffItem.java
@@ -0,0 +1,47 @@
+package stu.stones;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FireballEntity;
+import net.minecraft.entity.projectile.WindChargeEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class WindStaffItem extends Item {
+
+
+    public WindStaffItem(Settings settings){
+        super(settings);
+
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+
+        // This is to prevent desync.
+        if (world.isClient) {
+            return ActionResult.PASS;
+        }
+
+        // Spawn the wind charge.
+        WindChargeEntity ball = new WindChargeEntity(world,
+                user.getBlockPos().getX(),
+                user.getBlockPos().getY(),
+                user.getBlockPos().getZ(),
+                new Vec3d(user.getHorizontalFacing().getVector()));
+        world.spawnEntity(ball);
+
+        ItemStack staff = user.getStackInHand(hand);
+        staff.damage(1, user, EquipmentSlot.MAINHAND);
+
+        return ActionResult.SUCCESS;
+    }
+
+}
